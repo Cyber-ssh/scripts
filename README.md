277f34ae22969c8fb17884bfdfb962133140077a
J.F. Roque <123225396+Cyber-ssh@users.noreply.github.com>
2024-02-25 16:16:18 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Conteúdos
+
+• SSH PR0
+
+• SSH PLUS
+
+• SSH PREMIUM (Desativado)
+
+• PWEB PR0
+
+• PWEB PLUS - (Desativado)
+
+• ACESSO ROOT
+
+• CERTIFICADO SSL
+
+
+# SOLICITE KEY: 
+
+[![Join Telegram Chat](https://img.shields.io/badge/Join-Telegram%20Group-blue.svg?logo=Telegram)](https://t.me/srSPEEDiness)
+
+[![Join WhatsApp Chat](https://img.shields.io/badge/Join-WhatsApp%20Group-bl.svg?logo=WhatsApp)](https://wa.me/5521976102205)
+
+
+# __MULTI-SCRIPTS__
+
+```
+wget https://raw.githubusercontent.com/Cyber-ssh/scripts/main/install.sh; chmod +x install.sh; ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,137 @@
+
+barra="\033[0m\e[34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "/root/install.sh" > /bin/mko && chmod +x /bin/mko > /dev/null 2>&1
+
+##SCRIPTS A INSTALAR
+
+##TEAM-ILUUMINATI
+
+SSHPRO () {
+    bash <(wget -qO- https://cybercoari.com.br/master/sshplus)
+}
+SSHPLUS () {
+     wget install.speedcell.ga/Plus-ssh; chmod 777 Plus-ssh; ./Plus-ssh
+}
+SSHPREMIUM () {
+    wget https://raw.githubusercontent.com/install; chmod +x install; ./install
+}
+PWEBPRO () {
+    wget install.speedcell.ga/Pro-pweb; chmod 777 Pro-pweb; ./Pro-pweb
+}    
+PWEBPLUS () {
+    wget install.speedcell.ga/Plus-pweb; chmod 777 Plus-pweb; ./Plus-pweb
+}
+HABILITARROOT () {
+    wget -y; bash <(wget -qO- https://raw.githubusercontent.com/srSPEEDiness/SPEEDSSH-PLUS/master/senharoot.sh)
+}
+ATTSSL () {
+    apt install wget -y; wget --no-check-certificate https://www.dropbox.com/s/v2hvhv8z86zlsqd/ssl.sh; chmod +x ssl.sh; ./ssl.sh
+}
+LIMPARVPS () {
+    apt autoremove -y; apt -f install -y; apt autoclean -y; bash <(wget -qO- https://github.com/srSPEEDiness/SPEEDSSH-PLUS/raw/main/Install/remove.sh)
+}
+SINCPRO () {
+    bash <(wget -qO- https://cybercoari.com.br/cybercoari/sync/sincpainel.sh)
+}
+SINCPLUS () {
+    apt install wget -y; bash <(wget -qO- https://cybercoari.com.br/cybercoari/sync/sincpainel.sh)
+}
+MENU () {
+    clear
+    menu
+}
+
+while true $x != "ok"
+do
+clear
+
+echo -e "\033[0;34m╔══════════════════════════════════════════════════════════════╗\033[0m"
+echo -e "\033[1;31m \E[1;31;40m                   ⇱ VELOCIDADE E CONEXAO ⇲                  \E[0m \033[1;37m"
+echo -e "\033[0;34m╠══════════════════════════════════════════════════════════════╣\033[0m"
+echo -e "\033[00;34m║\033[1;31m\033[1;31m[\033[1;37m 01 •\033[1;31m]\033[1;37m ➩ \033[1;33m CYBERSSH\033[0;32m                                     \033[00;34m║"
+echo -e "\033[00;34m║\033[1;31m\033[1;31m[\033[1;37m 02 •\033[1;31m]\033[1;37m ➩ \033[1;33m SSH SPEED PLUS \033[0;32m                                    \033[00;34m║"
+echo -e "\033[00;34m║\033[1;31m\033[1;31m[\033[1;37m 03 •\033[1;31m]\033[1;37m ➩ \033[1;33m SPEED PREMIUM (Desativado) \033[0;32m                        \033[00;34m║"
+echo -e "\033[00;34m║\033[1;31m\033[1;31m[\033[1;37m 04 •\033[1;31m]\033[1;37m ➩ \033[1;33m PAINEL WEB SPEED PRO \033[0;32m                              \033[00;34m║"
+echo -e "\033[00;34m║\033[1;31m\033[1;31m[\033[1;37m 05 •\033[1;31m]\033[1;37m ➩ \033[1;33m PAINEL WEB SPEED PLUS \033[0;32m                             \033[00;34m║"
+echo -e "\033[00;34m║\033[1;31m\033[1;31m[\033[1;37m 06 •\033[1;31m]\033[1;37m ➩ \033[1;33m HABILITAR ROOT \033[0;32m                                    \033[00;34m║"
+echo -e "\033[00;34m║\033[1;31m\033[1;31m[\033[1;37m 07 •\033[1;31m]\033[1;37m ➩ \033[1;33m ATUALIZAR CERTIFICADO SSL \033[0;32m                         \033[00;34m║"
+echo -e "\033[00;34m║\033[1;31m\033[1;31m[\033[1;37m 08 •\033[1;31m]\033[1;37m ➩ \033[1;33m LIMPAR VPS \033[1;37m \033[0;32m                                       \033[00;34m║"
+echo -e "\033[00;34m║\033[1;31m\033[1;31m[\033[1;37m 09 •\033[1;31m]\033[1;37m ➩ \033[1;37m MENU \033[0;32m                                              \033[00;34m║"
+echo -e "\033[0;34m╠══════════════════════════════════════════════════════════════╣\033[0m"
+echo -e "\033[0;34m║\033[5;34;47m     A ➩ SINC PRO   ║    B ➩ SINC PLUS    ║    00 ➩ SAIR      \033[0m║\033[0m"
+echo -e "\033[0;34m╚══════════════════════════════════════════════════════════════╝\033[0m"
+echo -e ""
+echo -e "\033[0;34m╔══════════════════════════════════════════════════════════════╗\033[0m"
+echo -e "\033[1;31m \E[1;31;40m          COMPRE SUA KEY \033[1;36m(TELEGRAM)\033[1;31m: \033[1;37m@srSPEEDiness                  \E[0m \033[1;37m"
+echo -e "\033[0;34m╚══════════════════════════════════════════════════════════════╝\033[0m"
+echo -ne "\033[1;32m O QUE DESEJA FAZER \033[1;33m?\033[1;31m?\033[1;37m : "; read x
+
+case "$x" in 
+   1 | 01)
+   clear
+   SSHPRO
+   exit;
+   ;;
+   2 | 02)
+   clear
+   SSHPLUS
+   exit;
+   ;;
+   3 | 03)
+   clear
+   SSHPREMIUM
+   exit;
+   ;;
+   4 | 04)
+   clear
+   PWEBPRO
+   exit;
+   ;;
+   5 | 05)
+   clear
+   PWEBPLUS
+   exit;
+   ;;
+   6 | 06)
+   clear
+   HABILITARROOT
+   exit;
+   ;;
+   7 | 07)
+   clear
+   ATTSSL
+   exit;
+   ;;
+   8 | 08)
+   clear
+   LIMPARVPS
+   exit;
+   ;;
+   9 | 09)
+   clear
+   MENU
+   exit;
+   ;;
+   a | A)
+   clear
+   SINCPRO
+   exit;
+   ;;
+   b | B)
+   clear
+   SINCPLUS
+   exit;
+   ;;
+   0 | 00)
+   echo -e "\033[1;31mSaindo...\033[0m"
+   sleep 2
+   clear
+   exit;
+   ;;
+   *)
+   echo -e "\n\033[1;31mOpcao invalida !\033[0m"
+   sleep 2
+esac
+done
+}
+#fim
